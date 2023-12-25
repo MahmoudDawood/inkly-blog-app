ca8f3e5081808c813e1cf5ea7089b87dc38b69d3
Dude <mm.dawood166@gmail.com>
2023-12-25 22:51:26 +0200

    Store post in database

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Post;
 
 class BlogController extends Controller
 {
@@ -21,8 +24,30 @@ class BlogController extends Controller
     public function store(Request $request) {
         $request->validate([
             'title' => 'required',
-            'image' => 'required | image', // Has to be both (AND)
+            'image' => 'required | image', // Has to satisfy both (AND)
             'body' => 'required'
         ]);
+
+        $title = $request->input('title');
+        $slug = Str::slug($title, '-'); // To lower case and joins with second parameter.
+        $user_id = Auth::user()->id;
+        $body = $request->input('body');
+
+        // File upload
+        $imagePath = 'storage/' . $request->file('image')->store('postsImages', 'public');
+        // store method first parameter is the directory name in `storage/app/public`
+        // second parameter is the file system disk from `config/filesystems.php` {local, public, s3}
+            // As laravel exposes data in `public`, create a symbolic link to there
+        
+        $post = new Post();
+        $post->title = $title;
+        $post->slug = $slug;
+        $post->user_id = $user_id;
+        $post->body = $body;
+        $post->imagePath = $imagePath;
+        $post->save();
+
+        return redirect()->back()->with('status', 'Post Created Successfully');
+        // with stores key value pair in the session and sends it with the next response
     }
 }
