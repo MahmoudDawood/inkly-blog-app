38339c3a700f097190c89a7c0ed91d689a4c963e
Dude <mm.dawood166@gmail.com>
2023-12-27 22:37:41 +0200

    Create category resource controller

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,11 +18,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// To Welcome page
+// Welcome page
 Route::get('/', [WelcomeController::class, 'index'])->name('welcome.index');
 
 
-// To Blog page
+// Blog routes
 Route::get('/blog', [BlogController::class, 'index'])->name('blog.index'); // Get all posts
 Route::get('/blog/create', [BlogController::class, 'create'])->name('blog.create'); // Create post form
 Route::get('/blog/{post}/edit', [BlogController::class, 'edit'])->name('blog.edit'); // Edit post form
@@ -32,6 +33,10 @@ Route::post('/blog', [BlogController::class, 'store'])->name('blog.store'); // S
 Route::put('/blog/{post}', [BlogController::class, 'update'])->name('blog.update'); // Update post
 Route::delete('/blog/{post}', [BlogController::class, 'destroy'])->name('blog.destroy'); // Delete post
 
+
+// Categories routes (Created as a resource controller by `php artisan make:model ... -mrc` creating a migration, model, and controller)
+Route::resource('/categories', CategoryController::class); // Handles all requests
+// index, create, store, show, edit, update, destroy are implicitly handled.
 
 // About page (Using closure -anonymous function- for simple logic)
 Route::get('/about', function () {
